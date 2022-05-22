24caa93e02f51f83c1b7d5681068dc87a9b194e5
NastasiaFree <96425753+NastasiaFree@users.noreply.github.com>
2022-05-22 21:53:34 +0300

    Add files via upload

---
+++ scripts/catalog.js
@@ -0,0 +1,8 @@
+
+        let cards = document.querySelectorAll('.card');
+        console.log(cards);
+        for ( let card of cards) {
+            card.onclick = function(event) {
+                window.location.href = 'd1.html';
+            };
+}
